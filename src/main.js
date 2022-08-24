e9d189cc1b6fbc2569344be350be8cb0e572502a
Quincy Huang <quincyyhuang@gmail.com>
2022-08-23 20:12:44 -0400

    added updater

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 const { app } = require('electron');
+const { autoUpdater } = require("electron-updater");
 const { prepareIPC, makeMenu, createSettingsWindow } = require("./setup");
+
+// Set up auto updater
+if (app.isPackaged) {
+  autoUpdater.checkForUpdatesAndNotify();
+}
 
 // This fixes color brightness issues
 app.commandLine.appendSwitch('disable-color-correct-rendering');
